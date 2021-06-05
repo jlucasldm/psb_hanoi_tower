544e9e2f3b5f862fa8fe730f077e14d472ff1c45
Ariel <lima.ariel97@gmail.com>
2021-06-05 15:09:22 -0300

    release

--- projeto/hanoi.asm
+++ projeto/hanoi.asm
@@ -1,7 +1,8 @@
 SECTION .data
-n_discos db 3
+n_discos db 4
 
 SECTION .text
+extern printf
 global  _start
  
 ; Pseudocódigo da resolução da torre de hanói por recursão
@@ -38,7 +39,11 @@ _start:
     
 
     ; Executando a função recursiva
+    push ax
+    push bx
     call _print_inicio
+    pop bx
+    pop ax
     call _hanoi    
     call _print_final
     call _quit
@@ -66,7 +71,14 @@ _hanoi: ; hanoi(X, S, D, T) = hanoi(bh, al, ah, bl)
     pop ax
 
     ;--------------------print move X from S to D ----------------
+    push ax
+    push bx
+    movzx ecx, ah; coluna de destino
+    movzx edx, al ; coluna de saída
+    movzx edi, bh ; tamanho do disco
     call _print_mover ; Printa o movimento do disco
+    pop bx
+    pop ax
     
     ;----------------------hanoi(X - 1, T, D, S) -----------------
     ; Salvando contexto AL, AH, BL e BH
@@ -84,45 +96,55 @@ _hanoi: ; hanoi(X, S, D, T) = hanoi(bh, al, ah, bl)
     pop ax
     ;--------------------------------------------------------------    
 
-    jmp _ret 
-_if: ; Se número de discos é igual a 1.
-    call _print_mover
+    jmp _ret
+_if: ; Se número de discos é igual a 1. 
+    ;--------------------print move X from S to D ----------------
+    push ax
+    push bx
+    movzx ecx, ah; coluna de destino
+    movzx edx, al ; coluna de saída
+    movzx edi, bh ; tamanho do disco
+    pop bx
+    pop ax
+    call _print_mover ; Printa o movimento do disco
 _ret:
     ret
 
 
 _print_inicio:
     push string_inicio  ;inserindo à pilha a string a ser impressa
-    extern printf       ;chamada da função em C, já que windows não permite comando int
     call printf         ;itera sobre o topo da pilha e imprime a string
-    pop edx             ;remove a string da pilha
+    add esp, 4          ;move o ponteiro 4 bytes para apontar ao valor anterior da pilha
     ret
 
 string_inicio:
-    db "Torre de hanoi com tres discos", 0xa, 0 ; 0xa = newline, 0 = end of a C string.
+    db "Torre de Hanoi com três discos", 0xa, 0 ; 0xa = newline, 0 = end of a C string.
 
 _print_mover:
+    add ecx, 64
+    add edx, 64
+    push ecx
+    push edx
+    push edi
     push string_mover  ;inserindo à pilha a string a ser impressa
-    extern printf       ;chamada da função em C, já que windows não permite comando int
-    call printf         ;itera sobre o topo da pilha e imprime a string
-    pop edx             ;remove a string da pilha
+    call printf        ;itera sobre o topo da pilha e imprime a string
+    add esp, 16        ;move o ponteiro 4 * 4 bytes para apontar ao valor anterior da pilha 
     ret
 
 string_mover:
-    db "Movendo o disco %d da torre %d para a torre %d",0xa,0 ; 0xa = newline, 0 = end of a C string.
+    db "Movendo o disco %d da torre %c para a torre %c.",0xa,0 ; 0xa = newline, 0 = end of a C string.
 
 _print_final:
     push string_final  ;inserindo à pilha a string a ser impressa
-    extern printf       ;chamada da função em C, já que windows não permite comando int
     call printf         ;itera sobre o topo da pilha e imprime a string
-    pop edx             ;remove a string da pilha
+    add esp, 4          ;move o ponteiro 4 bytes para apontar ao valor anterior da pilha
     ret
 
 string_final:
-    db "Procedimento solucionado", 0xa, 0 ; 0xa = newline, 0 = end of a C string.
+    db "Procedimento solucionado.", 0xa, 0 ; 0xa = newline, 0 = end of a C string.
 
 
 _quit:
-    mov       rax, 60                
-    xor       rdi, rdi                
+    mov       eax, 1                
+    mov       ebx, 2                
     syscall 
